b33d661a368d62aae9ff2ffa637624553ffd5ca9
Patrick <p-l-g@hotmail.dk>
2021-05-25 23:03:11 +0200

    s

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request, redirect, session, url_for, escape, g
 import sqlite3
 from activity import Activity
-import pickle
 
 app = Flask(__name__)
 
-activitys = []
+activitys = {}
 
 predefined_activitys = [Activity("Undefined", "Fodboldkamp (U21) - Herning vs Silkeborg", "2.5", "DATO", "En fodboldkamp mellem Herning og Silkeborg. Spilles på Herningvej 2")]
 
@@ -14,22 +13,24 @@ app.secret_key = 'BAD_SECRET_KEY'
 def index():
     with sqlite3.connect("users.db") as db:
         try:
-            print(session['username'])
-            cursor = db.cursor()
-            cursor.execute("select * from Interests where username = '" + session['username'] + "'")
-            print(cursor.fetchall())
             if session.get('username') == None:
                 return render_template("login.html")
+            cursor = db.cursor()
+            cursor.execute("select * from Interests where username = '" + session['username'] + "'")
+            user_interests = cursor.fetchall()
+
+            cursor.execute("SELECT * FROM Activity WHERE user = '" + session['username'] + "'")
+            user_activitys = cursor.fetchall()
+            for row in user_activitys:
+                activitys[str(row[0])] = [row[0],row[1],row[2],row[3],row[4]]
             if request.method == 'POST':
-                global activitys
                 activity = request.form.get('activity')
                 time = request.form.get('time')
                 day = request.form.get('day')
                 description = request.form.get('description')
 
-                activitys.append(Activity(day,str(activity), str(time) ,"DATO",str(description)))
-                print(activitys)
-
+                cursor.execute("INSERT INTO Activity (activity, time, day, desc, user) VALUES (?,?,?,?,?)", (activity, time, day, description, session['username']))
+                return redirect("/")
             
 
         except sqlite3.Error:
@@ -37,23 +38,12 @@ def index():
             return render_template("index.html", error=message, activitys=activitys)
     return render_template("index.html", activitys=activitys)
 
-@app.route('/save', methods=['POST', 'GET'])
-def save():
-    with sqlite3.connect("users.db") as db:
-        try:
-            # Hvis der postes noget data
-            if request.method == "POST":
-                with open(f"datafiles/{session['username']}", 'wb') as file:
-                    pickle.dump(activitys, file)
-                return redirect("/")
-        except sqlite3.Error:
-            message = "There was a problem executing the SQL statement"
-            return render_template("index.html", error=message)
-
 @app.route('/profile', methods=['POST', 'GET'])
 def profile():
     with sqlite3.connect("users.db") as db:
         try:
+            if session.get('username') == None:
+                return render_template("login.html")
             if request.method == 'POST':
                 primary_interest = request.form.get('primary_interest')
                 secondary_interest = request.form.get('secondary_interest')
@@ -69,29 +59,8 @@ def profile():
             return render_template("profile.html", error=message)
     return render_template("profile.html")
 
-def log_the_user_in(username):
-    try:
-        with open(f"datafiles/{session['username']}", 'rb') as file2:
-            global activitys
-            saved_data = pickle.load(file2)
-            #activitys.append(saved_data)
-            activitys = saved_data
-            print(activitys)
-            #return render_template('index.html', activitys=activitys, username=username)
-            return redirect("/")
-    # Hvis personen ikke har fået oprettet en fil, opret en der er tom
-    except FileNotFoundError:
-        with open(f"datafiles/{session['username']}", 'wb') as file:
-            activitys = ""
-            pickle.dump(activitys, file)
-            with sqlite3.connect("users.db") as db:
-                try:
-                    cursor = db.cursor()
-                    cursor.execute("INSERT INTO Interests VALUES (?,?,?,?)", (session['username'], "Ikke oplyst", "Ikke oplyst", "Ikke oplyst"))
-                    return render_template('index.html', activitys=activitys, username=username)
-                except sqlite3.Error:
-                    message = "There was a problem executing the SQL statement"
-                    return render_template("login.html", error=message)
+def log_the_user_in():
+    return redirect("/")
     
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -110,7 +79,7 @@ def login():
                 if valid_login != []:
                     session['username'] = request.form['username']
                     
-                    return log_the_user_in(request.form['username'])
+                    return log_the_user_in()
                 else:
                     error = 'Invalid username/password'
         except sqlite3.Error:
@@ -136,6 +105,7 @@ def register():
                 if valid_login == []:
                     cur = db.cursor()
                     cur.execute("INSERT INTO User(username, password) values (?,?)", (username,password))
+                    cur.execute("INSERT INTO Interests VALUES (?,?,?,?)", (username, "Ikke oplyst", "Ikke oplyst", "Ikke oplyst"))
                     print(cur.fetchall())
                     
                     render_template('login.html', error=error)
